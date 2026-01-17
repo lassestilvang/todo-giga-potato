fce967a36e546e6d16627bc038612b5b8f94007c
Lasse Stilvang <lasse.stilvang@gmail.com>
2026-01-17 23:31:56 +0100

    refactor(utils): deduplicate classes in cn utility before merging

--- lib/utils.ts
+++ lib/utils.ts
@@ -2,5 +2,14 @@ import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  // Deduplicate classes before merging
+  const dedupedInputs = inputs.map(input => {
+    if (typeof input === "string") {
+      const classes = input.split(" ").filter(Boolean)
+      return [...new Set(classes)].join(" ")
+    }
+    return input
+  })
+  
+  return twMerge(clsx(dedupedInputs))
 }
